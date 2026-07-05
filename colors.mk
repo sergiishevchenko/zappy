eacb3581d0577308a53cc53213dcce1fefe6669e
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2026-07-05 20:28:36 +0200

    add colors.mk file to define terminal color codes for output formatting

---
+++ colors.mk
@@ -0,0 +1,24 @@
+ifndef COLORS_MK
+COLORS_MK = 1
+
+BOLD    := \033[1m
+RED     := \033[1;31m
+GREEN   := \033[1;32m
+YELLOW  := \033[1;33m
+BLUE    := \033[1;34m
+CYAN    := \033[1;36m
+MAGENTA := \033[1;35m
+RESET   := \033[0m
+
+ifeq ($(shell test -t 1 && echo tty),)
+BOLD    :=
+RED     :=
+GREEN   :=
+YELLOW  :=
+BLUE    :=
+CYAN    :=
+MAGENTA :=
+RESET   :=
+endif
+
+endif
